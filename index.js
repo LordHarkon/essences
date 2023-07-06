5c48e972d4a15cc5591a1261891e83efed043baf
LordHarkon <frez758@gmail.com>
2023-07-06 13:41:15 +0300

    added copy button to esssences

--- index.js
+++ index.js
@@ -17,26 +17,57 @@ fetch("./essences.json")
     const essences = await response.json();
     allEssences = essences;
     found = essences.length;
+
     for (let i = 0; i < 20; i++) {
       essenceContainer.innerHTML += essenceCard(essences[i]);
     }
+
+    addCopyButtons();
   })
   .then((json) => console.log(json));
 
 function essenceCard(essence) {
   return `
-    <div class="border border-white/20 px-2 py-1 hover:bg-slate-900 hover:border-white/40">
+    <div class="border border-white/20 px-2 py-1 hover:bg-slate-900 hover:border-white/40 relative">
       <h3 class="font-semibold text-lg">${essence.name}</h3>
       <p class="italic">${essence.description[0]}</p>
       <ul class="list-disc ml-5">
-        ${essence.description.map((ed, i) => (i !== 0 ? `<li>${ed}</li>` : null)).join("")}
+        ${essence.description
+          .filter((e, i) => (i === 0 ? false : true))
+          .map((ed) => `<li>${ed}</li>`)
+          .join("")}
       </ul>
+      <div id="copy-${
+        essence.id
+      }" class="absolute top-0 right-0 px-1 pb-1 pt-0.5 m-1 border border-white/20 hover:border-white/40 group"><i class="bi bi-clipboard text-zinc-300 group-hover:text-white"></i></div>
     </div>
   `;
 }
 
 function resetShown(length = 20) {
   shownEssences = length < 20 ? (length === 1 ? 1 : length - 1) : 20;
+}
+
+function addCopyButtons() {
+  const copyButtons = Array.from(document.querySelectorAll("[id^=copy-]"));
+  console.log(copyButtons);
+  copyButtons.forEach((copyButton) => {
+    copyButton.onclick = function () {
+      const id = copyButton.id.replace("copy-", "");
+      const essence = allEssences.find((es) => es.id === id);
+
+      console.log(`Clicked copy for ${essence.name} (${essence.id})`);
+
+      const text = `**${essence.name}**
+*${essence.description[0]}
+${essence.description
+  .filter((e, i) => (i === 0 ? false : true))
+  .map((ed) => `- ${ed}`)
+  .join("\n")}`;
+
+      navigator.clipboard.writeText(text);
+    };
+  });
 }
 
 function search(value = "", reset = true) {
@@ -68,6 +99,8 @@ function search(value = "", reset = true) {
   for (let i = 0; i <= (shownEssences === 1 ? 1 : shownEssences - 1); i++) {
     essenceContainer.innerHTML += essenceCard(foundEssences[i]);
   }
+
+  addCopyButtons();
 }
 
 searchInput.addEventListener("input", (e) => {
